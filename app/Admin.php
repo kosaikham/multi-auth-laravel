187aa9812c851ba783f55947a6bc10756b67ef3d
saikham <saikham.email@gmail.com>
2018-01-01 09:57:50 +0630

    first commit

---
+++ app/Admin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+//Notification for Seller
+use App\Notifications\AdminResetPasswordNotification;
+
+class Admin extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    //Send password reset notification
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new AdminResetPasswordNotification($token));
+    }
+}
